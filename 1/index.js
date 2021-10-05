f5fd6dde365d56c023480793b3fc3fc2aafc8867
gilbertomoj <gibamedeirosgc@gmail.com>
2021-10-05 20:27:01 -0300

    Atividades

---
+++ 1/index.js
@@ -0,0 +1,37 @@
+// 1)      Criar uma função de seta (arrow function) que recebe 2 parâmetros e retorna a multiplicação deles.
+
+// a)       Chamar a função e exibir o resultado no console.
+// let mult = (n1 , n2) =>{
+//     return n1 * n2
+// }
+// console.log(mult(3,4))
+ 
+
+// 2)      Criar uma função de seta (arrow function) que recebe três números como parâmetros , e retorna o maior número dos 3 valores.
+
+// a)       Chamar a função e exibir o resultado no console.
+
+//  let maior = (n1 , n2, n3) =>{
+//     if(n1 > n2 && n1 > n3){
+//         return n1
+//     }else if(n2 > n1 && n2 > n3){
+//         return n2
+//     }else if(n3 > n1 && n3 > n2){
+//         return n3
+//     }
+// }
+// console.log(maior(3,4, 5))
+ 
+
+// 3)      Criar uma função de seta (arrow function) que recebe um parâmetro inteiro e retorna a palavra ímpar ou par de acordo com o valor recebido.
+
+// a)       Chamar a função e exibir no console o resultado.
+// let retornaN = (n1) =>{
+//     if(n1 % 2 == 0){
+//         return "Par"
+//     }else{
+//         return "Ímpar"
+//     }
+// }
+
+// console.log(retornaN(11))
